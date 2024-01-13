09f7286a6362e529f1feb5f1bb99987b5d5aee95
Lucas Cesca <lucascesca14@gmail.com>
2024-01-13 15:01:46 -0300

    Project created

---
+++ src/test/java/com/lucascesca/webserviceproject/WebServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascesca.webserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
