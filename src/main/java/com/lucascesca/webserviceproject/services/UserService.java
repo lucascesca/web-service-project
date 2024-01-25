472d7eb1266954e98835b464fe75795ba63b20cd
Lucas Cesca <lucascesca14@gmail.com>
2024-01-24 22:01:04 -0300

    Exception handling - findById

--- src/main/java/com/lucascesca/webserviceproject/services/UserService.java
+++ src/main/java/com/lucascesca/webserviceproject/services/UserService.java
@@ -2,6 +2,7 @@ package com.lucascesca.webserviceproject.services;
 
 import com.lucascesca.webserviceproject.entities.User;
 import com.lucascesca.webserviceproject.repositories.UserRepository;
+import com.lucascesca.webserviceproject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,8 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
